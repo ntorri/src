585f0a2dca1c8730dd90653470f7f1a10e3d95ed
Nate Torri <nate@torridigital.com>
2015-06-11 14:14:04 -0600

    Naming

--- main/java/com/torridigital/creeperdude/Reference.java
+++ main/java/com/torridigital/creeperdude/Reference.java
@@ -2,6 +2,6 @@ package com.torridigital.creeperdude;
 
 public class Reference {
 	public static final String MOD_ID = "creeperdude";
-	public static final String MOD_NAME = "CreeperDude's Mod";
+	public static final String MOD_NAME = "CreeperDude's Mayhem Mod";
 	public static final String VERSION = "1.0";
 }
